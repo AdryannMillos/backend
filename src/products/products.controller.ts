be2dd6289dcdfc9377f9f29819dabcb110e3d514
Adryann Millos <adryann312@gmail.com>
2022-05-24 21:26:17 -0300

    tem(Product) Add Crud

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,9 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param } from '@nestjs/common';
+import { ProductsService } from './products.service';
 
 @Controller('products')
 export class ProductsController {
-  @Post()
-  addProduct(): any {
+  constructor(private readonly productsService: ProductsService) {}
 
-}
+  @Post()
+  addProduct(
+    @Body('title') prodTitle: string,
+    @Body('description') prodDescription: string,
+    @Body('price') prodPrice: number,
+  ): any {
+    const generatedId = this.productsService.insertProduct(
+      prodTitle,
+      prodDescription,
+      prodPrice,
+    );
+    return { id: generatedId };
+  }
+
+  @Get()
+  getAllProducts(): any {
+    return this.productsService.fetchProduct();
+  }
+
+  @Get(':id')
+  getSingleProducts(@Param('id') productId: string): any {
+    return this.productsService.fetchSingleProduct(productId);
+  }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,3 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Product } from './product.model';
 
-export class ProductService {}
+@Injectable()
+export class ProductsService {
+  private products: Product[] = [];
+
+  insertProduct(title: string, description: string, price: number) {
+    const productId = Math.random().toString();
+    const newProduct = new Product(productId, title, description, price);
+    this.products.push(newProduct);
+    return productId;
+  }
+
+  fetchProduct() {
+    return [...this.products];
+  }
+
+  fetchSingleProduct(id: string) {
+    const product = this.products.find((item) => item.id === id);
+    if (!product) {
+      throw new NotFoundException('product not found');
+    }
+    return { ...product };
+  }
+}
